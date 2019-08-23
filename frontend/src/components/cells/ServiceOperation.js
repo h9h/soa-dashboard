0841bf1dd672a50ab36774ab9eecc2c2a951c890
Michael Heinke (SVI) <michael.heinke@sv-informatik.de>
2019-08-23 18:29:38 +0200

    Cell-Component ServiceOperation verallgemeinert

--- frontend/src/components/cells/ServiceOperation.js
+++ frontend/src/components/cells/ServiceOperation.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { Smaller } from '../styles'
 
 const ServiceOperation = ({row}) => {
+  const so = Array.isArray(row.value) ? row.value : row.value.split(':').map(t => t.trim())
   return (
     <>
-      <Smaller key={0}>{row.value[0]}</Smaller>
+      <Smaller key={0}>{so[0]}</Smaller>
       <br/>
-      <Smaller key={1}>{row.value[1]}</Smaller>
+      <Smaller key={1}>{so[1]}</Smaller>
     </>
   )
 }
