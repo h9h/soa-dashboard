10a53e8d222355b7ffe87ab2123a13187751ff1a
Michael Heinke (SVI) <michael.heinke@sv-informatik.de>
2020-04-06 11:02:46 +0200

    Namen der Attribute korrigiert

--- frontend/src/logic/actionHandlers/utils.js
+++ frontend/src/logic/actionHandlers/utils.js
@@ -32,31 +32,35 @@ export async function getQueuename (execute, MESSAGE, OPERATION, SENDERFQN) {
   } catch (_) {
     fields = {senderFQN: MESSAGE.senderFQN}
   }
-  if (!fields.senderFQN) return getFehlermeldung(fields.senderFQN, SENDERFQN, 'senderFQN nicht ermittelbar')
+  if (!fields.senderFQN) {
+    return getFehlermeldung(fields.senderFQN, SENDERFQN, 'senderFQN nicht ermittelbar')
+  }
 
   const values = await getModelvalue(MODELS.SENDERFQN_2_QUEUENAME, fields.senderFQN)
-  if (!values) return getFehlermeldung(fields.senderFQN, SENDERFQN, 'Keine Werte zu SenderFQN modelliert')
+  if (!values) {
+    return getFehlermeldung(fields.senderFQN, SENDERFQN, 'Keine Werte zu SenderFQN modelliert')
+  }
 
   if (typeof values === 'string') {
     execute.setValue('queuename', values)
     return {success: true, result: {operation: OPERATION, senderFQN: SENDERFQN, queuename: values}}
   } else {
-    if (values.queuename) {
-      execute.setValue('queuename', values.queuename)
+    if (values.QueueName) {
+      execute.setValue('queuename', values.QueueName)
     }
-    if (values.topicname) {
-      execute.setValue('topicname', values.topicname)
+    if (values.TopicName) {
+      execute.setValue('topicname', values.TopicName)
     }
 
-    if (!values.queuename && !values.topicname) return getFehlermeldung(fields.senderFQN, SENDERFQN, 'Queue-/Topicname nicht modelliert')
+    if (!values.QueueName && !values.TopicName) return getFehlermeldung(fields.senderFQN, SENDERFQN, 'Queue-/Topicname nicht modelliert')
 
     return {
       success: true,
       result: {
         operation: OPERATION,
         senderFQN: SENDERFQN,
-        queuename: values.queuename,
-        topicname: values.topicname
+        queuename: values.QueueName,
+        topicname: values.TopicName
       }
     }
   }
