e0c1b5ece0da6e7f7ba35a08e5f4d445b00011d9
Michael Heinke (SVI) <michael.heinke@sv-informatik.de>
2020-11-12 16:06:15 +0100

    Überflüssige Ermittlung des Queuenamens beim Löschen von Messages entfernt

--- frontend/src/logic/actionHandlers/deleteMessage.js
+++ frontend/src/logic/actionHandlers/deleteMessage.js
@@ -6,30 +6,25 @@
 import Log from '../../log'
 import { deleteMessage, doNotFindMessage, getMessage } from '../api/api-dashboard'
 import Executor from './Executor'
-import { errorHandler, getQueuename } from './utils'
+import { errorHandler } from './utils'
 
 const log = Log('deletemessages')
 
 export const deleteMessages = async (record) => {
   log.trace('Record', record)
-  const { ID, ENVIRONMENT, OPERATION, MESSAGE, SENDERFQN } = record
+  const { ID, ENVIRONMENT } = record
   const { messageType } = record.filter
 
   const execute = new Executor('Delete Message')
 
   try {
     /* 1 */
-    await execute.step('ermittle Queuename', async () => {
-      return await getQueuename(execute, MESSAGE, OPERATION, SENDERFQN)
-    })
-
-    /* 2 */
     await execute.step('get Message', () => getMessage(ENVIRONMENT, messageType, ID))
 
-    /* 3 */
+    /* 2 */
     await execute.step('delete Message', () => deleteMessage(ENVIRONMENT, messageType, ID))
 
-    /* 4 */
+    /* 3 */
     await execute.step('check Message deleted', () => doNotFindMessage(ENVIRONMENT, messageType, ID))
 
   } catch (err) {
